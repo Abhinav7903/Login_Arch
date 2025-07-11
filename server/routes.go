30d7796abf2fef22dcd5d3f4938ddfd7b6d0edbb
abhinava31 <abhinava@hornet.technology>
2025-07-11 14:29:59 +0530

    add option:

--- server/routes.go
+++ server/routes.go
@@ -6,9 +6,9 @@ func (s *Server) RegisterRoutes() {
 	s.router.HandleFunc("/ping", s.HandlePong()).Methods(http.MethodGet)
 
 	// User routes
-	s.router.HandleFunc("/users", s.HandleCreateUser()).Methods(http.MethodPost)
-	s.router.HandleFunc("/users", s.HandleGetUser()).Methods(http.MethodGet)
-	s.router.HandleFunc("/login", s.HandleLogin()).Methods(http.MethodPost)
+	s.router.HandleFunc("/users", s.HandleCreateUser()).Methods(http.MethodPost, http.MethodOptions)
+	s.router.HandleFunc("/users", s.HandleGetUser()).Methods(http.MethodGet, http.MethodOptions)
+	s.router.HandleFunc("/login", s.HandleLogin()).Methods(http.MethodPost, http.MethodOptions)
 }
 
 func (s *Server) HandlePong() http.HandlerFunc {
